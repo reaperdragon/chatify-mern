fc06608f4b9a6b20588518d0a1fb4bba15c620a0
aakrut <aakrutdabhi@gmail.com>
2022-06-27 19:48:42 +0530

    favicons

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getUserFromLocalStorage } from "./localStorage";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API,
 });
 
 api.interceptors.request.use((config) => {
